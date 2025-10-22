32a4b385d4fee51ef9c5f284a45fedd70af94270
masato_nitta <masato.nitta@g.softbank.co.jp>
2025-10-22 16:37:55 +0900

    コードの更新と整理

--- go-app/pkg/graph/model/model.go
+++
@@ -1,6 +0,0 @@
-package model
-
-// This file can be used for custom model definitions
-// gqlgen will generate models_gen.go for GraphQL types
-
-// Additional custom models can be added here if needed
